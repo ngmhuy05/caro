735c6bbbdb719e62805643c8811b90e6608eb705
JackStar2 <yellowstarmc@gmail.com>
2025-10-30 23:56:23 +0700

    Enhance Game data structure with winningCells
    Added winningCells to Game data structure to track winning positions.

--- temp/GameState.hs
+++ temp/GameState.hs
@@ -1,3 +1,4 @@
+-- File: GameState.hs
 module GameState where
 
 import Data.List (transpose)
@@ -19,14 +20,16 @@ data GameStatus = Running | Win Player | Draw deriving (Eq, Show, Read)
 data Game = Game {
     board         :: Board,
     currentPlayer :: Player,
-    gameStatus    :: GameStatus
+    gameStatus    :: GameStatus,
+    winningCells  :: [(Int, Int)] -- <<< THEM TRUONG NAY DE LUU 5 O THANG
 } deriving (Show, Read)
 
 initialGame :: Game
 initialGame = Game {
     board = replicate boardSize (replicate boardSize Empty),
     currentPlayer = X,
-    gameStatus = Running
+    gameStatus = Running,
+    winningCells = [] -- <<< THEM GIA TRI BAN DAU
 }
 
 nextPlayer :: Player -> Player
